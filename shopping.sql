10e432ffb6384e6ecbcdf3cbc3d10d4b3aabba09
redkueen <xqh20186@163.com>
2017-08-21 22:16:05 +0800

    where 过滤行，按user_id 分组（group by）,having 过滤行

--- shopping.sql
+++ shopping.sql
@@ -1,5 +1,5 @@
 SHOW CREATE TABLE `order`;
-UPDATE  users SET PASSWORD ='xyz12345',jifen = '4356' WHERE user_id = 1;
+UPDATE  users SET `PASSWORD` ='xyz12345',jifen = '4356' WHERE user_id = 1;
 INSERT users(NAME,PASSWORD,jifen)VALUE('曹桂林','cgl23416',23433);
 INSERT goods(NAME,price)VALUE('鸡爪',20);
 DELETE FROM goods WHERE good_id = 3;
@@ -16,3 +16,7 @@ SELECT goods.name FROM `order`,goods WHERE order.good_id = goods.good_id AND ord
 
 SELECT users.name,goods.name,goods.price FROM `order` JOIN goods ON order.good_id = goods.good_id 
 JOIN users ON order.user_id = users.user_id WHERE order.user_id = 1;
+SELECT MAX(price)AS mp,MIN(price)AS mip FROM goods;
+SELECT user_id ,COUNT(id)AS COUNT,MAX(summation)AS ms,AVG(summation)AS av FROM `order` WHERE good_id=4 GROUP BY user_id;
+SELECT user_id ,COUNT(id)AS COUNT,MAX(summation)AS ms,AVG(summation)AS av FROM `order` WHERE good_id=4 GROUP BY user_id HAVING av >25; 
+
