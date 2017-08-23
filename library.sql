77396203463629993e6bce80a71304211640ef6b
redkueen <xqh20186@163.com>
2017-08-24 00:07:13 +0800

    order by, group by

--- library.sql
+++ library.sql
@@ -7,7 +7,7 @@ INSERT worker(NAME) VALUES('徐巧红');
 INSERT worker(NAME) VALUES('徐巧红');
 
 `worker`
-CREATE TABLE borrow_return(id INT outo_increament primery_key,book_id INT,reader_id INT);
+CREATE TABLE borrow_return(id INT auto_increment primary key,book_id INT,reader_id INT);
 INSERT worker(NAME,number) VALUE('张三','234156');
 INSERT reader(NAME,PASSWORD,ID_card)VALUES('徐巧红','13579','32456788900876543');
 CREATE TABLE good222(id INT AUTO_INCREMENT PRIMARY KEY,NAME VARCHAR(20),price INT);
--- shopping.sql
+++ shopping.sql
@@ -19,4 +19,53 @@ JOIN users ON order.user_id = users.user_id WHERE order.user_id = 1;
 SELECT MAX(price)AS mp,MIN(price)AS mip FROM goods;
 SELECT user_id ,COUNT(id)AS COUNT,MAX(summation)AS ms,AVG(summation)AS av FROM `order` WHERE good_id=4 GROUP BY user_id;
 SELECT user_id ,COUNT(id)AS COUNT,MAX(summation)AS ms,AVG(summation)AS av FROM `order` WHERE good_id=4 GROUP BY user_id HAVING av >25; 
+ALTER TABLE `order`ADD CONSTRAINT FOREIGN KEY fk_good_id (good_id) REFERENCES goods(good_id);
+SELECT * FROM goods ORDER BY  price;
+SELECT * FROM goods ORDER BY  price DESC;
+SELECT * FROM goods ORDER BY  price ASC;
 
+
+SELECT * FROM `order` WHERE user_id = 1 ORDER BY summation;
+
+SELECT * FROM goods LIMIT 5;
+
+SELECT * FROM goods LIMIT 2,4;
+
+SELECT * FROM `order` WHERE user_id = 1 ORDER BY good_id ,summation;
+SELECT * FROM `order`  ORDER BY good_id ,summation;
+SELECT * FROM `order` ORDER BY good_id DESC,summation ASC;
+
+
+
+
+SELECT user_id ,summation,COUNT(id)AS COUNT,MAX(summation)AS ms,AVG(summation)AS av FROM `order` WHERE good_id=4 GROUP BY user_id,summation;
+
+
+
+SELECT user_id ,summation,COUNT(id)AS COUNT,MAX(summation)AS ms,AVG(summation)AS av FROM `order` GROUP BY user_id,summation;
+
+
+SELECT user_id ,good_id,COUNT(id)AS COUNT,MAX(summation)AS ms,AVG(summation)AS av FROM `order` WHERE good_id=4 GROUP BY user_id,good_id;
+
+SELECT user_id ,good_id,COUNT(id)AS COUNT,MAX(summation)AS ms,AVG(summation)AS av FROM `order` GROUP BY user_id,good_id;
+
+
+CREATE TABLE `市` (                                                                                                                                           
+`省名` VARCHAR(45) NOT NULL,                                                                                                                               
+`市名` VARCHAR(45) NOT NULL,                                                                                                                               
+PRIMARY KEY (`省名`,`市名`)                                                                                                                              
+ );
+ 
+ CREATE TABLE `市2` (  																		
+id INT PRIMARY KEY,																
+`省名` VARCHAR(45) NOT NULL,                                                                                                                               
+`市名` VARCHAR(45) NOT NULL,                                                                                                                               
+UNIQUE(`省名`,`市名`)                                                                                                                              
+ ); 
+ 
+  CREATE TABLE `市3` (  																		
+id INT PRIMARY KEY,																
+`省名` VARCHAR(45) NOT NULL,                                                                                                                               
+`市名` VARCHAR(45) NOT NULL,                                                                                                                               
+CONSTRAINT uq_省名_市名 UNIQUE(`省名`,`市名`)                                                                                                                              
+ ); 
