3e4d110a8f6871038f0930f51055a1b6210a1551
redkueen <xqh20186@163.com>
2017-08-26 12:30:44 +0800

    bank 数据库：开始事务（start transaction），回滚（rollback），提交（commit），设置自动提交关闭（set autocommit = 'OFF' ）

--- transaction/bank.sql
+++ transaction/bank.sql
@@ -19,9 +19,33 @@ start transaction;
 update user set balance=balance-10000 where id =1;
 update user set balance=balance+10000 where id =2;
 select * from user;
-commit;
+commit;---提交 代表事务结束；  事务正常提交了，这是一种事务结束
+
+start transaction;    ---开始 “事务”
+update user set balabce=balance+10000 where id =1;
+update user set balance=balance-10000 where id =2;
+rollback;  --- “回滚 or 撤销”   ；  一般出现错误时候，事物回滚，这也是一种事务结束
+
 
 start transaction;
 update user set balabce=balance+10000 where id =1;
 update user set balance=balance-10000 where id =2;
+-- 如果 id为2的user的balance小于0:
+rollback;  
+-- 否则:
+commit;
+
+
+start transaction;
+-- 如果 id为1的user的balance小于10000:
+rollback;
+-- 否则: {做减去 和 加上
+update user set balabce=balance-10000 where id =1;
+update user set balance=balance+10000 where id =2;
+commit;
+-- }
+
+set autocommit = 'OFF';  -- 设置“自动提交”关闭
+update user set balance = balance - 10000 where id = 1;
+update user set balance = balance + 10000 where id = 2;
 rollback;
