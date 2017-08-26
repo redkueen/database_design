649d416afe5e55c0ea15fa08089ad854b578de66
redkueen <xqh20186@163.com>
2017-08-26 22:42:10 +0800

    。。。

--- shopping.sql
+++ shopping.sql
@@ -10,16 +10,20 @@ INSERT `order`(user_id,good_id,summation)VALUE(2,4,40);
 SELECT * FROM users WHERE user_id = 1;
 SELECT * FROM goods WHERE good_id = 2;
 SELECT `name` FROM goods WHERE good_id=2;
+
 SELECT `name` FROM goods WHERE good_id IN (SELECT good_id FROM `order` WHERE user_id =1);
 SELECT goods.name FROM `order` JOIN goods ON order.good_id = goods.good_id WHERE user_id =2; 
 SELECT goods.name FROM `order`,goods WHERE order.good_id = goods.good_id AND order.user_id = 1;
 
 SELECT users.name,goods.name,goods.price FROM `order` JOIN goods ON order.good_id = goods.good_id 
 JOIN users ON order.user_id = users.user_id WHERE order.user_id = 1;
+
 SELECT MAX(price)AS mp,MIN(price)AS mip FROM goods;
 SELECT user_id ,COUNT(id)AS COUNT,MAX(summation)AS ms,AVG(summation)AS av FROM `order` WHERE good_id=4 GROUP BY user_id;
 SELECT user_id ,COUNT(id)AS COUNT,MAX(summation)AS ms,AVG(summation)AS av FROM `order` WHERE good_id=4 GROUP BY user_id HAVING av >25; 
+
 ALTER TABLE `order`ADD CONSTRAINT FOREIGN KEY fk_good_id (good_id) REFERENCES goods(good_id);
+
 SELECT * FROM goods ORDER BY  price;
 SELECT * FROM goods ORDER BY  price DESC;
 SELECT * FROM goods ORDER BY  price ASC;
