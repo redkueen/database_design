6c79ab00e23fd8134a1ce982acf65dcdebfb35e5
redkueen <xqh20186@163.com>
2018-08-24 20:02:43 +0800

    视图view

---
+++ 视图view.sql
@@ -0,0 +1,10 @@
+﻿-- view 视图
+CREATE VIEW v_student_count_per_course AS 
+SELECT `name` ,student_number FROM 
+        course JOIN (SELECT c_id , COUNT(id) AS student_number FROM  student_course  GROUP BY c_id)t
+                  ON course.id = t.c_id ;
+SELECT * FROM v_student_count_per_course t;
+SELECT * FROM ( SELECT `name` ,student_number FROM 
+        course JOIN (SELECT c_id , COUNT(id) AS student_number FROM  student_course  GROUP BY c_id)t
+                  ON course.id = t.c_id  )t;
+DELETE FROM v_student_count_per_course; -- 该视图为多表join,不能删除。 故报错误
